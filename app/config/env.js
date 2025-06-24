5578d67ce9eb688e86ae544eadb09f5059da4107
Paresh Patel <pareshkapdiya@gmail.com>
2025-06-24 23:42:55 +0530

    Update env.js

--- app/config/env.js
+++ app/config/env.js
@@ -1,10 +1,10 @@
 // Put your secret key here.
 exports.Captcha_secretKey = "6Ld6SKsUAAAAAOju2_kZhuD3V10ExDJRsAYOxUUA";
 
-exports.host =  'mail.weldtechindustries.com';
-exports.user = 'info@weldtechindustries.com';
-exports.pass = 'weldtech@123';
-exports.sub = 'Enquiry from customer'
+exports.host = "mail.weldtechindustries.com";
+exports.user = "info@weldtechindustries.com";
+exports.pass = "Weldtech@123";
+exports.sub = "Enquiry from customer";
 
 // exports.host =  'mail.omyinfotech.com';
 // exports.user = 'ujjval@omyinfotech.com';
