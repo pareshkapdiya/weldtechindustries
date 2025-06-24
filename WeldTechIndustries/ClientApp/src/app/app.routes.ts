356fecd5ea038bca57aaf3a4c3d5823ff53dd333
Paresh Patel <pareshkapdiya@gmail.com>
2025-06-24 22:48:00 +0530

    Removed all files

--- WeldTechIndustries/ClientApp/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { ErrorComponent } from './pages/components/error/error.component';
-
-export const routes: Routes = [
-      { path: 'error', component: ErrorComponent },
-      { path: '**', redirectTo: 'error' },
-];
